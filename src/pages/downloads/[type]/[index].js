e41cf8348c7040e6f9ad13e01436003722cf774a
Ruben Gianelli <rubengianelli8@gmail.com>
2022-09-14 20:52:15 -0300

    fix imports

--- src/pages/downloads/[type]/[index].js
+++ src/pages/downloads/[type]/[index].js
@@ -1,11 +1,11 @@
 import dynamic from "next/dynamic";
 import Download from "@/containers/client/download";
 
-const Download = dynamic(() => import("../../containers/client/download"), {
+const Downloaded = dynamic(() => import("../../containers/client/download"), {
   ssr: false,
 });
 const index = () => {
-  return <Download />;
+  return <Downloaded />;
 };
 
 export default index;
