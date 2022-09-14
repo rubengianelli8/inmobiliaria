3073c0e32866a2bc9d27907059eed8f5d4f2c6a0
Ruben Gianelli <rubengianelli8@gmail.com>
2022-09-14 20:47:53 -0300

    fix

--- src/pages/downloads/[type]/[index].js
+++ src/pages/downloads/[type]/[index].js
@@ -1,51 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { PDFDownloadLink } from "@react-pdf/renderer";
-import { useLazyQuery } from "@apollo/client";
-import { GET_RECEIPT_BY_ID } from "@/gql/queries/receipt.gql";
+import dynamic from "next/dynamic";
+import Download from "@/containers/client/download";
 
-import { FaDownload } from "react-icons/fa";
-
-import DocPdf from "@/containers/doc-pdf.js";
-import Router from "next/router";
-import DownloadPdf from "@/containers/receipt/DownloadPdf";
-
+const Download = dynamic(() => import("../../containers/client/download"), {
+  ssr: false,
+});
 const index = () => {
-  const index = Router.router?.query?.index;
-  const type = Router.router?.query?.type;
-
-  const [getReceiptById, { data }] = useLazyQuery(GET_RECEIPT_BY_ID);
-  useEffect(() => {
-    if (type && type !== "receipt") {
-      Router.push("/404");
-    }
-    if (index && type === "receipt") {
-      getReceiptById({
-        variables: {
-          receiptId: parseInt(Buffer.from(index, "base64").toString("ascii")),
-        },
-      });
-    }
-  }, [type, index]);
-  return (
-    <>
-      {data?.getReceipt && (
-        <div className="w-screen h-screen flex flex-col justify-center items-center">
-          <h2 className="text-20 underline mb-3">Aquí esta tu recibo</h2>
-          <PDFDownloadLink
-            document={<DocPdf receipt={data.getReceipt} />}
-            fileName={`recibo-${data.getReceipt.client.user.last_name}-${data.getReceipt.date}.pdf`}
-          >
-            <button className="flex items-center gap-x-2 text-primary bg-tertiary p-3 rounded font-bold text-25 hover:bg-primary hover:text-tertiary">
-              Descargar Recibo
-              <span>
-                <FaDownload className="text-25" />
-              </span>
-            </button>
-          </PDFDownloadLink>
-        </div>
-      )}
-    </>
-  );
+  return <Download />;
 };
 
 export default index;
